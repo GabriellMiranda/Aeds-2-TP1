fc75a1a517cc176b8745f311987c8d283b44902a
Gabriel Miranda <70436348+Gabriel090699@users.noreply.github.com>
2021-08-19 16:21:28 -0300

    upadtepat

---
+++ Pat.h
@@ -0,0 +1,39 @@
+#ifndef PAT_H
+#define PAT_H
+
+typedef enum{
+    interno, externo
+} TipoNo; // Identificar se o no é externo ou interno.
+//Estrutura de dados Patricia
+typedef struct TipoPatNo{
+    TipoNo Tipono;
+    union{ 
+      struct{
+        unsigned char indice; // essa variável guarda o indice em que o no interno se difere
+        char compara; // essa variável guadra o caracter diferente, presente no indice acima
+        struct TipoPatNo *Esq, *Dir;
+    } NoInterno;
+
+    char *chave; // ponteiro para char que aponta para a estring que está sendo armazenada, se o no for externo
+    }PatNo;
+} TipoPatNo;
+
+char Bit(unsigned char i, char k[]);
+
+short NoEExterno(TipoPatNo *No);
+
+TipoPatNo *criaNoInt(int i, char c, TipoPatNo **Esq, TipoPatNo **Dir);
+
+TipoPatNo *criaNoExt(char *string);
+
+void SearchPat (char k[], TipoPatNo *no);
+
+TipoPatNo *InsereEntre(char k[], char compara, TipoPatNo **no, int i);
+
+TipoPatNo *Insere(char k[], TipoPatNo **no);
+
+//int ContaPalavras(TipoPatNo *no);
+
+void ImprimePalavras(TipoPatNo *no);
+
+#endif //PAT_H
--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "Pat.h"
 
 int main(){
-
+    
+    TipoPatNo *Arvore = NULL;
+    Arvore = Insere("gabriel", &Arvore);
+    Arvore = Insere("Erike", &Arvore);
+    ImprimePalavras(Arvore);
     return 0;
 }
